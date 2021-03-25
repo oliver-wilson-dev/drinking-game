a67283ce6e5672bcd4128ca63d9891a4f4592fdb
Oliver Wilson <contact.oliver.wilson@gmail.com>
2021-03-25 21:50:27 +0000

    linting: further remove unused constant

--- src/state/reducers/index.test.js
+++ src/state/reducers/index.test.js
@@ -43,25 +43,6 @@ describe('reducer', () => {
       });
     });
 
-    describe(`when called with action type ${TOGGLE_THEME}`, () => {
-      const action = {
-        type: TOGGLE_THEME
-      };
-      const state = {
-        [Symbol('test-key')]: Symbol('test-value'),
-        theme: DARK_THEME
-      };
-
-      describe('and the theme is not the light theme', () => {
-        it('should return the correct state, with the theme set to the light theme', () => {
-          expect(reducer(state, action)).toEqual({
-            ...state,
-            theme: LIGHT_THEME
-          });
-        });
-      });
-    });
-
     describe('when the action type is unknown', () => {
       const state = { [Symbol('test-key')]: Symbol('test-value') };
       const action = { type: 'SOME_UNKNOWN_ACTION' };
