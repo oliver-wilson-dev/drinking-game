f0cea863876fb3b84eb6ca86bfd96ba807d26fdf
Oliver Wilson <contact.oliver.wilson@gmail.com>
2021-04-01 23:34:36 +0100

    feat: scroll to top on navigation

--- src/components/Page/Page.jsx
+++ src/components/Page/Page.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import cn from 'classnames';
 import styles from './Page.module.css';
+import { useScrollTop } from '../../hooks';
 
-const Page = ({ children, className }) => (
-  <div className={cn(styles.page, className)}>
-    {children}
-  </div>
-);
+const Page = ({ children, className }) => {
+  useScrollTop();
+
+  return (
+    <div className={cn(styles.page, className)}>
+      {children}
+    </div>
+  );
+};
 
 export default Page;
