92aa09fca261efcd2c7fc4cec429623a6c36d848
Oliver Wilson <contact.oliver.wilson@gmail.com>
2021-04-01 23:33:24 +0100

    feat: add some more questions

--- server/Questions/constants.js
+++ server/Questions/constants.js
@@ -1,13 +1,17 @@
 const questions = [
-  'What is the best colour?',
-  'Is the moon made out of cheese?',
-  'If You Had Three Wishes, What Would You Wish For?',
-  'What’s The Most Beautiful Place You’ve Ever Seen?',
-  'What’s Your Favorite TV Show?',
-  'What’s The Strangest Thing In Your Refrigerator?',
-  'Have You Ever Been To A Five Star Resort?',
-  'Would You Rather Have 10 Hobbies Or One Passion?',
-  'Would You Rather Have 10 Hobbies Or One Passion? What’s Your Favorite TV Show? If You Had Three Wishes, What Would You Wish For?'
+  'Who? What? When? Where?. The next person to ask a question drinks 4 times',
+  'Would you rather have a dog with a cat’s personality or a cat with a dog’s personality? Losing category drinks 3 times',
+  'The youngest person in the room gives out 2 sips',
+  'Would you rather win one million dollars today or 10 million dollars in ten years? Losing category drinks 2 times',
+  'Would you rather sell all of your possessions or one of your organs? Losing category drinks 4 times',
+  'Would you rather win the lottery or live twice as long? Losing category drinks 4 times',
+  'Categories: UK Counties. Go round listing UK counties. First person to fail drinks 3 times',
+  'Poker face time. The next person to smile drinks 4 times',
+  'Drink twice if you think ALL of the other players are cool',
+  'Categories! Name a category, then go round listing things related to that category. First person to fail drinks 2 times',
+  'Would you rather stop using Instagram or stop using YouTube? Losing category drinks 4 times',
+  "Drink twice if you've ever got a speeding ticket",
+  'Categories: American States. Go round listing American States. First person to fail drinks 1 time',
 ];
 
 module.exports = { questions };
