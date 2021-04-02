d1768c808272e4c9d1f1913af55fd6818dbac3b6
Oliver Wilson <contact.oliver.wilson@gmail.com>
2021-04-02 14:34:02 +0100

    fix: websocket connection failing due to cors.. I think

--- server/Server/index.js
+++ server/Server/index.js
@@ -3,7 +3,14 @@ const express = require('express');
 const app = express();
 const httpServer = require('http').createServer(app);
 
-const options = { cors: { origin: '*' } };
+const options = {
+  cors: {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204
+  }
+};
 const io = require('socket.io');
 
 class Server {
